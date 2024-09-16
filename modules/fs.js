ab4f6e4f9be20bdcbb016e3262085edeab836ba3
Arian Botine <arianbotine@hotmail.com>
2024-09-15 21:31:29 -0300

    Importando e demonstrando o uso da biblioteca fs para manipulação de arquivo

---
+++ modules/fs.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.mkdir(path.join(__dirname, "test"), (error) => {
+  if (error) {
+    return console.log("Erro: ", error);
+  }
+  console.log("Pasta criada com sucesso!");
+
+  fs.writeFile(
+    path.join(__dirname, "test", "test.txt"),
+    "hello node!",
+    (error) => {
+      if (error) {
+        return console.log("Erro: ", error);
+      }
+      console.log("Arquivo criado com sucesso!");
+    }
+  );
+});
+
+fs.writeFile(
+  path.join(__dirname, "test", "test.txt"),
+  "sobrescrever!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+    console.log("Arquivo criado com sucesso!");
+    fs.appendFile(
+      path.join(__dirname, "test", "test.txt"),
+      "hello world!",
+      (error) => {
+        if (error) {
+          return console.log("Erro: ", error);
+        }
+        console.log("Arquivo modificado com sucesso!");
+
+        fs.readFile(
+          path.join(__dirname, "test", "test.txt"),
+          "utf8",
+          (error, data) => {
+            if (error) {
+              console.log("Erro: ", error);
+            }
+            console.log(data);
+          }
+        );
+      }
+    );
+  }
+);
