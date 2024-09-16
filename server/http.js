2269e33435122d4d76449ab0cd5f50603cc64570
Arian Botine <arianbotine@hotmail.com>
2024-09-15 21:49:22 -0300

    Criando um retorno de json para uma requisição

--- server/http.js
+++ server/http.js
@@ -7,6 +7,21 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>home page</h1>");
   }
+
+  if (req.url === "/users") {
+    const users = [
+      {
+        name: "Arian Botine",
+        email: "arian@botine.com",
+      },
+      {
+        name: "Bruna Ramos",
+        email: "bruna@ramos.com",
+      },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
 });
 
 server.listen(port, () => {
