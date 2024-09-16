6c8f6829be95046c33d14cf23327e287bcaf9d28
Arian Botine <arianbotine@hotmail.com>
2024-09-15 21:42:51 -0300

    Criando um exemplo de requisição simples usando http

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Person } = require("./person");
 //require("./modules/path");
 //require("./modules/fs");
 
+require("./server/http");
+
 const person = new Person("Arian");
 
 console.log(person.sayMyName());
---
+++ server/http.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>home page</h1>");
+  }
+});
+
+server.listen(port, () => {
+  console.log("Escutando na porta ", port);
+});
